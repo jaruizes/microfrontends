9abff1bdf8981dc75096dbb0bbdcc0ad0eb5c8a2
jaruizes <jalb80@gmail.com>
2020-09-28 13:59:55 +0200

    Integrated wc-shortcuts

--- microfrontends/mf-global-position/src/app/components/global-position/global-position.component.ts
+++ microfrontends/mf-global-position/src/app/components/global-position/global-position.component.ts
@@ -9,6 +9,25 @@ export class GlobalPositionComponent implements OnInit {
 
   public urlCardsSummary = 'http://microfrontends-cdn.s3-website.eu-west-2.amazonaws.com/mf-cards-summary/main.js';
   public urlAccountsSummary = 'http://microfrontends-cdn.s3-website.eu-west-2.amazonaws.com/mf-accounts-summary/main.js';
+  public urlShortcuts = 'http://microfrontends-cdn.s3-website.eu-west-2.amazonaws.com/web-components/wc-shortcuts/wc-shortcuts.bundled.js';
+  public shortcuts = [
+    {
+      icon: 'unfold_less',
+      text: 'Internal transfer'
+    },
+    {
+      icon: 'payments',
+      text: 'Transfer'
+    },
+    {
+      icon: 'person',
+      text: 'Profile'
+    },
+    {
+      icon: 'groups',
+      text: 'Bizum'
+    }
+  ];
 
   constructor() { }
 
