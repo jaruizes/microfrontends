0a7281b518384b960b6e4fade7ba38676b867a70
jaruizes <jalb80@gmail.com>
2020-10-14 17:01:34 +0200

    Fixed responsive behaviour

--- web-components/items-table/src/components/items-table/items-table.tsx
+++ web-components/items-table/src/components/items-table/items-table.tsx
@@ -45,32 +45,20 @@ export class ItemsTable {
     return (
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">{this.header}</h5>
+          <h5 class="card-title items-title">{this.header}</h5>
           <div class="container-fluid items">
             {this.items.map((item) =>
-              <div class="row" style={{padding: "0em"}}>
-                <div class="col-12">
-                  <div class="card items-card" onClick={() => this.itemClickThrowEvent(item)}>
-
-                      <div class="card-body" style={{padding: "0em", cursor: "pointer"}}>
-                        <div class="container-fluid">
-                          <div class="row align-middle">
-                          <div class="col-md-8 col-sm-12">
-                          <div class="items-data">
-                            <p class="text-muted text-left align-bottom items-date">{item.header}</p>
-                            <p class="text-left h5 items-title1">{item.title1}</p>
-                            <p class="h6 text-muted text-left"><em>{item.subtitle1}</em></p>
-                          </div>
-                          </div>
-                          <div class="col-md-4 col-sm-12 text-right my-auto" style={{"padding-right": "2em", "padding-top": "0.5em"}}>
-                            <p class="h3 items-title2" style={{"margin": "0em"}}>{item.title2}</p>
-                            <p class="h4 text-muted" style={{"margin": "0em"}}><em>{item.subtitle2}</em></p>
-                          </div>
-                          </div>
-                        </div>
-                      </div>
-
+              <div class="row item-row" style={{padding: "0em"}}>
+                <div class="col-md-8 col-xs-8 col-8 items-left">
+                  <div class="items-data">
+                    <p class="text-muted text-left align-bottom items-date">{item.header}</p>
+                    <p class="text-left h5 items-title1">{item.title1}</p>
+                    <p class="h6 text-muted text-left items-subtitle1"><em>{item.subtitle1}</em></p>
                   </div>
+                </div>
+                <div class="col-md-4 col-md-4 col-4 text-right my-auto items-right">
+                  <p class="h3 items-title2" style={{"margin": "0em"}}>{item.title2}</p>
+                  <p class="h4 text-muted items-subtitle2" style={{"margin": "0em"}}><em>{item.subtitle2}</em></p>
                 </div>
               </div>
             )}
