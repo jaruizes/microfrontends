5720ef35c3c440765a53b3d5a8865346a396449c
jaruizes <jalb80@gmail.com>
2020-11-16 00:34:50 +0100

    Added test for i18n and events management

--- microfrontends/mf-account-detail/cypress/integration/tests.js
+++ microfrontends/mf-account-detail/cypress/integration/tests.js
@@ -3,6 +3,7 @@ const formatIBAN = (account) => {
 };
 
 const NUM_ELEMENTS = 4;
+let generalChannel;
 
 describe('Micro Frontend Test Suite', function () {
 
@@ -14,12 +15,18 @@ describe('Micro Frontend Test Suite', function () {
         cy.server();
         cy.route('GET', '/api/config/account-detail-mf-config', 'fixture:account-detail-mf-config.json');
         cy.route('GET', '/api/accounts/1', 'fixture:account-1.json');
+
+        generalChannel = new BroadcastChannel("microfrontends");
     });
 
     it('should be up & running, including all the components', () => {
         cy.visit('/');
         cy.contains('Account Detail', {includeShadowDom: true});
         cy.contains('Movements', {includeShadowDom: true});
+        cy.contains('Nickname', {includeShadowDom: true});
+        cy.contains('Holder', {includeShadowDom: true});
+        cy.contains('IBAN', {includeShadowDom: true});
+        cy.contains('Balance', {includeShadowDom: true});
     });
 
     it('should contain account detail', () => {
@@ -40,6 +47,22 @@ describe('Micro Frontend Test Suite', function () {
             cy.wrap($el).contains(this.data['movements'][index]['account']);
             cy.wrap($el).contains(this.data['movements'][index]['amount']);
         });
+    });
+
+    it('should be translated if a change locale event is sent', () => {
+        generalChannel.postMessage({
+            cmd: 'changeLocale',
+            payload: {
+                locale: 'es'
+            }
+        });
+
+        cy.contains('Detalle de cuenta', {includeShadowDom: true});
+        cy.contains('Movimientos', {includeShadowDom: true});
+        cy.contains('Alias', {includeShadowDom: true});
+        cy.contains('Titular', {includeShadowDom: true});
+        cy.contains('IBAN', {includeShadowDom: true});
+        cy.contains('Saldo', {includeShadowDom: true});
     });
 
 });
